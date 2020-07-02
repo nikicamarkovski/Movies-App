d370a1c09addbaa282624401cbf6e87c1ada61eb
Nikica <markovskinikica@gmail.com>
2020-07-02 16:30:35 +0200

    migrations and dbContext

---
+++ aspnetapp/Controllers/MovieControler.cs
@@ -0,0 +1,7 @@
+namespace aspnetapp.Controllers
+{
+    public class MovieControler
+    {
+        
+    }
+}
---
+++ aspnetapp/Models/Movies.cs
@@ -0,0 +1,11 @@
+
+
+namespace aspnetapp.Models
+{
+    public class Movies
+    {
+      public int Id {get; set;}
+      public string Name {get ; set;}
+      
+    }
+}
